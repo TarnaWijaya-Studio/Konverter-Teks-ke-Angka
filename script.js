1c29511acdf4a76f0549e3b8ff092ad9e0ed6e2b
Tarna <158784522+TarnaWijaya@users.noreply.github.com>
2025-01-31 20:56:11 +0700

    update script.js

--- script.js
+++ script.js
@@ -8,9 +8,11 @@ function convertToNumbers() {
     } else if (char >= 'a' && char <= 'z') {
       outputText += (char.charCodeAt(0) - 96) + ' ';
     } else if (char >= 'A' && char <= 'Z') {
-      outputText += (char.charCodeAt(0) - 64) + ' ';
+      outputText += `A${char.charCodeAt(0) - 64} `;
+    } else if (char >= '0' && char <= '9') {
+      outputText += `N${char} `;
     } else {
-      outputText += `[${char.codePointAt(0)}]`;
+      outputText += `U+${char.codePointAt(0).toString(16).toUpperCase()} `;
     }
   }
 
@@ -20,15 +22,19 @@ function convertToNumbers() {
 function convertToText() {
   const inputText = document.getElementById('inputText').value.trim();
   let outputText = '';
-  let matches = inputText.match(/\d+|.*?| /g);
+  let matches = inputText.match(/A\d+|\d+|N\d|U\+\w+| /g);
 
   for (let match of matches) {
     if (match === ' ') {
       outputText += ' ';
     } else if (/^\d+$/.test(match)) {
       outputText += String.fromCharCode(parseInt(match) + 96);
-    } else if (/^\d+$/.test(match)) {
-      outputText += String.fromCodePoint(parseInt(match.replace(/|/g, '')));
+    } else if (/^A\d+$/.test(match)) {
+      outputText += String.fromCharCode(parseInt(match.substring(1)) + 64);
+    } else if (/^N\d$/.test(match)) {
+      outputText += match.substring(1);
+    } else if (/^U\+\w+$/.test(match)) {
+      outputText += String.fromCodePoint(parseInt(match.substring(2), 16));
     }
   }
 
